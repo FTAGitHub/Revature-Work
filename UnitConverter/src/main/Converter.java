b3602aca1e3dc6fd03bb4ace86a834544fb8240d
FTAGitHub <86728612+FTAGitHub@users.noreply.github.com>
2021-06-30 15:59:41 -0400

    Add files via upload

---
+++ UnitConverter/src/main/Converter.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.Scanner;
+
+public class Converter {
+	Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args){
+        int menuSelection;
+        
+        System.out.println("Please select an option from those listed below:\n");
+        System.out.println("1. Inches to Centimeters");
+        System.out.println("2. Fahrenheit to Celcius");
+        System.out.println("3. Gallons to Liters");
+        System.out.println("4. Quit");
+	}
+}
