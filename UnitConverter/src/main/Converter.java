edc24a9ce3ce5f29c8104629ea0a6795e4fce6cd
FTAGitHub <86728612+FTAGitHub@users.noreply.github.com>
2021-07-02 00:50:50 -0400

    Add files via upload

--- UnitConverter/src/main/Converter.java
+++ UnitConverter/src/main/Converter.java
@@ -6,12 +6,44 @@ public class Converter {
 	Scanner scanner = new Scanner(System.in);
 	
 	public static void main(String[] args){
-        int menuSelection;
+		Scanner scanner = new Scanner(System.in);
+        int menuSelection = 0;
+        
+        while (menuSelection != 1) {
         
         System.out.println("Please select an option from those listed below:\n");
         System.out.println("1. Inches to Centimeters");
         System.out.println("2. Fahrenheit to Celcius");
         System.out.println("3. Gallons to Liters");
         System.out.println("4. Quit");
+        
+        int menu = scanner.nextInt();
+        
+        
+        switch (menu) {
+        case 1: System.out.println("Insert the inches you want converted to centimeters: ");
+        double inch = scanner.nextDouble();
+        double cm = inch * 2.54;
+        System.out.println(inch + " inch(es) is " + cm + " centimeters.");
+        break;
+        
+        case 2: System.out.println("Insert the temperature in Fahrenheit you want converted to Celsius: ");
+        double fahrenheit = scanner.nextDouble();
+        double celsius = (fahrenheit - 32) * 5/9;
+        System.out.println(fahrenheit + " degrees Fahrenheit is " + celsius + " degrees Celsius.");
+        break;
+        
+        case 3: System.out.println("Enter the number of gallons you want converted to liters: ");
+        double gallons = scanner.nextDouble();
+        double liters = gallons * 3.78541;
+        System.out.println(gallons + " gallons is " + liters + " liter(s).");
+        break;
+        case 4: System.out.println("Goodbye!");
+        menuSelection = 1;
+        break;
+        }
+        
+        }
+        scanner.close();
 	}
 }
