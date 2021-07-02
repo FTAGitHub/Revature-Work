a7802779ffd39f77f98073dfc7eae112978cdc58
FTAGitHub <86728612+FTAGitHub@users.noreply.github.com>
2021-07-02 10:36:21 -0400

    Add files via upload

--- UnitConverter/src/main/Converter.java
+++ UnitConverter/src/main/Converter.java
@@ -3,7 +3,6 @@ package main;
 import java.util.Scanner;
 
 public class Converter {
-	Scanner scanner = new Scanner(System.in);
 	
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
